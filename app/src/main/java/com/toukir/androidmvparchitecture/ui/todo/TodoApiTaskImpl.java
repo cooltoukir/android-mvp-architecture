fbb038af694e4dbd608f85e76d707bc8e24c5700
Toukir Ahmed <cooltoukir@gmail.com>
2025-12-30 13:04:26 +0600

    Use TodoApiTask not TodoApiTaskImpl

--- app/src/main/java/com/toukir/androidmvparchitecture/ui/todo/TodoApiTaskImpl.java
+++ app/src/main/java/com/toukir/androidmvparchitecture/ui/todo/TodoApiTaskImpl.java
@@ -13,7 +13,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public abstract class TodoApiTaskImpl implements TodoApiTask {
+public class TodoApiTaskImpl implements TodoApiTask {
 
     private final TodoContract.View mTodoView;
 
--- app/src/main/java/com/toukir/androidmvparchitecture/ui/todo/TodoPresenter.java
+++ app/src/main/java/com/toukir/androidmvparchitecture/ui/todo/TodoPresenter.java
@@ -1,17 +1,19 @@
 package com.toukir.androidmvparchitecture.ui.todo;
 
-public class TodoPresenter extends TodoApiTaskImpl implements TodoContract.Presenter {
+public class TodoPresenter implements TodoContract.Presenter {
 
     private TodoContract.View mTodoView;
+    private final TodoApiTask todoApiTask;
 
     public TodoPresenter(TodoContract.View mTodoView) {
-        super(mTodoView);
+//        super(mTodoView);
         this.mTodoView = mTodoView;
+        this.todoApiTask = new TodoApiTaskImpl(mTodoView);
     }
 
     @Override
     public void onTodoTask() {
         mTodoView.showLoader();
-        onTodoApiTask();
+        todoApiTask.onTodoApiTask();
     }
 }
